22ac50cae1931f2946315f4a198166ab163e21aa
Y0s3p <jfgarrido14@gmail.com>
2018-03-12 15:00:48 +0100

    Update 12/03/2018

--- src/invasionMarciana/ReadFile.java
+++ src/invasionMarciana/ReadFile.java
@@ -14,8 +14,7 @@ public class ReadFile {
 
     private String[] files;
     private int ai;
-
-    ArrayList<String> aux = new ArrayList<>();
+    private ArrayList<String> auxiliar = new ArrayList<>();
 
     public ReadFile(){
 
@@ -25,10 +24,16 @@ public class ReadFile {
     }
 
 
+    /**
+     *La clase reedFiles se encarga de leer el fichero que contiene los datos con los que trabajaremos asi como el almacenar estos datos en el Array files.
+     *
+     * @throws FileNotFoundException
+     */
     public void reedFiles() throws FileNotFoundException{
 
         Scanner sc = new Scanner( new File("C:\\Users\\Yosep\\Desktop\\ficheros\\invasion.txt"));
 
+        //Se va a encargar leer el objeto sc ademas de almacenar los datos que contiene en files mientras haya una siguiente linea en el.
         while (sc.hasNextLine()){
 
             files[ai] = sc.nextLine();
@@ -40,17 +45,24 @@ public class ReadFile {
 
     }
 
+    /**
+     * orderFiles creara el ArrayList el cual alamacenara los datos que contiene file. Luego llamaremos al metodo sort de la clase Collection dentro de Comparator implementada
+     * en la clase MyComparator, a la cual le pasaremos el ArrayList auxiliar para que lo ordene.
+     * Una vez ordenado vamos a crear el Array subfile inicializado a la longitud del ArrayList.
+     * A continuacion le indicamos que subfile sera igual a metodo toArray de auxiliar.
+     * Por ultimo realizamos un bucle para mostrar el contenido de subfiles.
+     */
     public void orderFiles(){
 
-        aux = new ArrayList<>(Arrays.asList(files));
+        auxiliar = new ArrayList<>(Arrays.asList(files));
 
-        Collections.sort(aux, new MyComparator());
+        Collections.sort(auxiliar, new MyComparator());
 
-        String[] files2 = new String[aux.size()];
-        files2 = aux.toArray(files2);
+        String[] subFiles = new String[auxiliar.size()];
+        subFiles = auxiliar.toArray(subFiles);
 
-        for (int x = 0; x < files2.length; x++){
-            System.out.println(files2[x]);
+        for (int x = 0; x < subFiles.length; x++){
+            System.out.println(subFiles[x]);
         }
 
     }
